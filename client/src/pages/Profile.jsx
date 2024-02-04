bc68a50cdde838f710bd599fa3d177f07438a7a4
Igor Carlos <igor.lima58@gmail.com>
2024-02-04 17:44:59 +0800

    listing page with image slider using swiper package

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -187,12 +187,12 @@ export default function Profile() {
         <h1 className='text-center mt-7 text-2xl font-semibold'>Your Listings</h1>
         {userListings.map((listing) =>
           <div key={listing._id} className='border rounded-lg p-3 flex justify-between items-center gap-4'> 
-            <Link to={`/listings/${listing._id}`}>
+            <Link to={`/listing/${listing._id}`}>
               <img src={listing.imageUrls[0]} alt='listing cover' 
               className='h-16 w-16 object-contain'/>
             </Link>
             <Link className='text-slate-700 font-semibold flex-1 hover:underline truncate'
-            to={`/listings/${listing._id}`}>
+            to={`/listing/${listing._id}`}>
               <p>{listing.name}</p>
             </Link>
             <div className='flex flex-col item-center'>
