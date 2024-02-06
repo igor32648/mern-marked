57d1d0e701f8563d2ca9d32c4d01c9842ba0fd40
Igor Carlos <igor.lima58@gmail.com>
2024-02-07 01:50:11 +0800

    home and about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,6 +2,13 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>About</div>
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-semibold mb-4 text-slate-800'>
+        About Cabedelo Market
+      </h1>
+      <p className='mb-4 text-slate-700'>Welcome to Cabedelo Market, your ultimate destination for all things local and fresh. As you step into our virtual marketplace, prepare to be enchanted by a vibrant array of products sourced directly from the heart of Cabedelo. From succulent fruits and vegetables harvested from nearby farms to artisanal crafts crafted with passion and care, we offer a diverse selection that celebrates the rich culture and flavors of our region.</p>
+      <p className='mb-4 text-slate-700'>At Cabedelo Market, we strive to create a seamless shopping experience that connects you with the best our community has to offer. Our user-friendly interface allows you to browse through our extensive catalog with ease, while our secure payment system ensures peace of mind with every transaction. Whether you're searching for everyday essentials or unique treasures, you'll find it all here at Cabedelo Market.</p>
+      <p className='mb-4 text-slate-700'>Join us in supporting local growers, artisans, and entrepreneurs by choosing Cabedelo Market as your go-to online marketplace. With each purchase, you not only discover exceptional products but also contribute to the growth and sustainability of our community. Explore Cabedelo Market today and embark on a journey of discovery, where every purchase tells a story and every interaction celebrates the spirit of local commerce.</p>
+    </div>
   )
 }
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,156 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css/bundle';
+import { Navigation } from 'swiper/modules';
+import SwiperCore from 'swiper';
+import ListingItem from '../components/ListingItem';
 
 export default function Home() {
+  const [offerListings, setOfferListings] = useState([]);
+  const [saleListings, setSaleListings] = useState([]);
+  const [rentListings, setRentListings] = useState([]);
+  SwiperCore.use([Navigation]);
+  console.log(offerListings);
+  console.log(saleListings);
+  console.log(rentListings);
+  useEffect(() => {
+    const fetchOfferListings = async () => {
+      try {
+        const res = await fetch('/api/listing/get?offer=true&limit=4');
+        const data = await res.json();
+        setOfferListings(data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    const fetchRentListings = async () => {
+      try {
+        const res = await fetch('/api/listing/get?type=rent&limit=4');
+        const data = await res.json();
+        setRentListings(data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    const fetchSalesListings = async () => {
+      try {
+        const res = await fetch('/api/listing/get?type=sale&limit=4');
+        const data = await res.json();
+        setSaleListings(data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchOfferListings();
+    fetchRentListings();
+    fetchSalesListings();
+  }, []);
   return (
-    <div>Home</div>
+    <div>
+      {/* top */}
+      <div className='flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto'>
+        <h1 className='text-slate-700 font-bold text-3xl
+        lg:text-6xl'>
+          Find your <span className='text-slate-500'>dream</span> home 
+          <br />
+          right here
+        </h1>
+        <div className='text-gray-400 text-sm sm:text-sm'>
+          Cabedelo Market is the place you were looking for to 
+          find your next place to live.
+          <br />
+          We have any kind of place you dreamed of.
+        </div>
+        <Link to={'/search'} className='text-xs sm:text-sm text-blue-800
+        font-bold hover:underline'>
+          Let's get started
+        </Link>
+      </div>
+      {/*swiper */}
+      <Swiper navigation>
+        {
+          offerListings && offerListings.length > 0 &&
+            offerListings.map((listing) => (
+              <SwiperSlide key={listing._id}>
+                <div className='h-[500px]' 
+                style={{background: `url(${listing.imageUrls[0]}) center 
+                no-repeat`, backgroundSize:'cover'}}>
+
+                </div>
+              </SwiperSlide>
+            ))
+        }
+      </Swiper>
+      {/* listings */}
+      <div className='max-w-6xl mx-auto p-3 flex flex-col gap-8 my-8'>
+          {
+            offerListings && offerListings.length > 0 && (
+              <div className=''>
+                <div className='my-3'>
+                  <h2 className='text-2xl font-semibold text-slate-600'>
+                    Recent Offers
+                  </h2>
+                  <Link to={'/search?offer=true'}
+                  className='text-sm text-blue-800 hover:underline'>
+                    Show more offers
+                  </Link>
+                </div>
+                <div className='flex flex-wrap gap-4'>
+                  {
+                    offerListings.map((listing) => (
+                      <ListingItem key={listing._id} listing={listing}/>
+                    ))
+                  }
+                </div>
+              </div>  
+            )
+          }
+          {
+            rentListings && rentListings.length > 0 && (
+              <div className=''>
+                <div className='my-3'>
+                  <h2 className='text-2xl font-semibold text-slate-600'>
+                    Recent places for rent
+                  </h2>
+                  <Link to={'/search?type=rent'}
+                  className='text-sm text-blue-800 hover:underline'>
+                    Show more places for rent
+                  </Link>
+                </div>
+                <div className='flex flex-wrap gap-4'>
+                  {
+                    rentListings.map((listing) => (
+                      <ListingItem key={listing._id} listing={listing}/>
+                    ))
+                  }
+                </div>
+              </div>  
+            )
+          }
+          {
+            saleListings && saleListings.length > 0 && (
+              <div className=''>
+                <div className='my-3'>
+                  <h2 className='text-2xl font-semibold text-slate-600'>
+                    Recent places for sale
+                  </h2>
+                  <Link to={'/search?type=sale'}
+                  className='text-sm text-blue-800 hover:underline'>
+                    Show more for sale
+                  </Link>
+                </div>
+                <div className='flex flex-wrap gap-4'>
+                  {
+                    saleListings.map((listing) => (
+                      <ListingItem key={listing._id} listing={listing}/>
+                    ))
+                  }
+                </div>
+              </div>  
+            )
+          }
+      </div>
+    </div>
   )
 }
